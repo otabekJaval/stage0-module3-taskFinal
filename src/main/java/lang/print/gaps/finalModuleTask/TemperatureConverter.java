4a2faa8673a4e3b4d8eaf6cbc74cb8e4e93863df
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-02 16:32:04 +0500

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+
+
+    public  void toFahrenheit(int temperatureCelsius) {
+        System.out.println((temperatureCelsius * (float)9 / 5) + 32);
     }
 }
