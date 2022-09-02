a161f214fe96f3173de16dc11c1b377d049c0a08
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-02 16:30:29 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
+
     public void calculateSum(int number) {
+        int print = 0;
+        while (number > 0) {
+            print += number % 10;
+            number = number / 10;
+        }
+        System.out.println(print);
     }
+
+
 }
