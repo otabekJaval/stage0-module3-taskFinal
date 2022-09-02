942d0da440e3515c3d058b25db0c0867334de74d
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-02 16:31:05 +0500

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String res = "";
+        while (number > 0) {
+            res += number % 10;
+            number /= 10;
+        }
+        System.out.println(res);
     }
+
+
 }
